1ffa1bb73bc90413be1eb8a04c8e088b454a9722
yuritetsu <mrr.samson@gmail.com>
2024-01-18 19:46:27 -0800

    last commit

--- calculator.js
+++ calculator.js
@@ -1,11 +1,32 @@
-let userName = ();
-if (userName === true ) {
-    console.log(`Hello <user>!`);
+let userName = "";
+if (userName) {
+    console.log(`Hello ${userName}!`);
 }
-else (userName === false ) {
-    console.log(`Hello, guest!`)
+else {
+    console.log(`Hello, guest!`);
 }
 
-let variableOne = 20;
-let variableTwo = 35;
-let mathOp = ("add","subtract","multiply");
+
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+let operation = prompt ("Choose a math operation you would like to do (add, subtract, multiply, divide, modulus):");
+
+if (operation === "add") {
+    result = num1 + num2;
+    console.log(`Sum of ${num1} and ${num2} is ${result}.`);
+} else if (operation === "subtract") {
+    result = num1 - num2;
+    console.log(`Difference of ${num1} and ${num2} is ${result}.`);
+} else if (operation === "multiply") {
+    result = num1 * num2;
+    console.log(`Product of ${num1} and ${num2} is ${result}.`);
+} else if (operation === "divide") {
+    result = num1 / num2;
+    console.log(`Division of ${num1} and ${num2} is ${result}.`);
+} else if (operation === "modulus") {
+    result = num1 % num2;
+    console.log(`Modulus of ${num1} and ${num2} is ${result}.`);
+} else {
+    console.log(`${operation} is an invalid operation.`);
+}
+
